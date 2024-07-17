1f10329ddee0de36325a6535c49147fca6312f72
Kuluru Vineeth <kuluruvineeth8623@gmail.com>
2024-07-17 06:56:31 +0530

    removed aws secret keys

--- frontend/lib/ecs.ts
+++ frontend/lib/ecs.ts
@@ -3,8 +3,8 @@ import { CreateServiceCommand, ECSClient } from "@aws-sdk/client-ecs";
 const ecsClient = new ECSClient({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "AKIAZQ3DOPGVTMHESALR",
-    secretAccessKey: "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",
+    accessKeyId: "",
+    secretAccessKey: "",
   },
 });
 
